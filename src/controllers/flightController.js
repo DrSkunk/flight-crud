2044ee14cfcbdd1a6ad7dc32103a7e6c9faad802
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2025-04-08 14:02:33 +0200

    init flight controller with CRUD operations

---
+++ src/controllers/flightController.js
@@ -0,0 +1,17 @@
+import { Flight } from "../models/Flight.js";
+
+// Get all flights
+export async function getFlights() {}
+
+// Get flight by ID
+export async function getFlightById() {}
+
+// Create new flight
+export async function createFlight() {}
+
+// Update a flight
+export async function updateFlight() {}
+
+// Cancel a  a flight
+// This does not delete the flight, but updates its status to 'cancelled'
+export async function cancelFlight() {}
