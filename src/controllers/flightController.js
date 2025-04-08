bffbea52e5c718c2b56cc6fda2752f61128714c1
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2025-04-08 14:15:19 +0200

    implement flightController functions - Update model for flightNumber to be an index and main ID

--- src/controllers/flightController.js
+++ src/controllers/flightController.js
@@ -1,17 +1,91 @@
 import { Flight } from "../models/Flight.js";
 
 // Get all flights
-export async function getFlights() {}
+export async function getFlights() {
+	const flights = await Flight.find();
+	return flights;
+}
 
-// Get flight by ID
-export async function getFlightById() {}
+// Get flight by Flight number
+export async function getFlightByFlightNumber(flightNumber) {
+	const flight = await Flight.findOne({ flightNumber });
+
+	if (!flight) {
+		throw new Error("Flight not found");
+	}
+
+	return flight;
+}
 
 // Create new flight
-export async function createFlight() {}
+export async function createFlight(flightData) {
+	const {
+		flightNumber,
+		departure,
+		destination,
+		departureTime,
+		arrivalTime,
+		aircraft,
+	} = flightData;
+
+	const flight = await Flight.create({
+		flightNumber,
+		departure,
+		destination,
+		departureTime,
+		arrivalTime,
+		aircraft,
+	});
+	if (!flight) {
+		throw new Error("Flight creation failed");
+	}
+	return flight;
+}
 
 // Update a flight
-export async function updateFlight() {}
+export async function updateFlight(flightData) {
+	const {
+		flightNumber,
+		departure,
+		destination,
+		departureTime,
+		arrivalTime,
+		aircraft,
+	} = flightData;
+
+	const flight = await Flight.findOneAndUpdate(
+		{ flightNumber },
+		{
+			departure,
+			destination,
+			departureTime,
+			arrivalTime,
+			aircraft,
+		},
+		// new: true will return the updated document
+		// runValidators: true will run the validators on the updated document
+		{ new: true, runValidators: true },
+	);
+
+	if (!flight) {
+		throw new Error("Flight not found");
+	}
+
+	return flight;
+}
 
 // Cancel a  a flight
 // This does not delete the flight, but updates its status to 'cancelled'
-export async function cancelFlight() {}
+export async function cancelFlight(flightNumber) {
+	const flight = await Flight.findOneAndUpdate(
+		{ flightNumber },
+		{ status: "cancelled" },
+		{ new: true },
+	);
+
+	if (!flight) {
+		throw new Error("Flight not found");
+	}
+
+	return flight;
+}
--- src/models/Flight.js
+++ src/models/Flight.js
@@ -8,6 +8,8 @@ const flightSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 			trim: true,
+			unique: true,
+			index: true,
 		},
 		departure: {
 			type: String,
@@ -41,15 +43,7 @@ const flightSchema = new mongoose.Schema(
 	{
 		// Adds createdAt and updatedAt timestamps
 		timestamps: true,
-		// Enable id field to be used instead of _id
-		toJSON: { virtuals: true },
-		toObject: { virtuals: true },
 	},
 );
-
-// Add a virtual property for 'id' that transforms _id to id
-flightSchema.virtual("id").get(function () {
-	return this._id.toString();
-});
 
 export const Flight = mongoose.model("Flight", flightSchema);
